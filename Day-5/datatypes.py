ab93cb2a37481729e195eb5e7ce1dff1fc52513b
mounikamoru <mounikamoru4634@gmail.com>
2026-03-27 15:14:05 +0530

    Add files via upload

---
+++ Day-5/datatypes.py
@@ -0,0 +1,106 @@
+# Variable naming examples
+myvar = 10
+my_var = 10
+My_var = 10
+My_Var = 10
+_myvar = 10
+myvar12 = 10
+
+# Case sensitivity
+myvar = 10
+Myvar = 20
+
+# Data types
+followers = 1000
+likes_cnt = 56
+followers = 15.6
+price = 67.356
+
+a = 3 + 6j
+a = 5 + 7j
+
+# Strings
+s = 'python programming'
+s = "Python"
+product_name = "laptop"
+
+# String indexing
+s[0]
+s[1]
+s[2]
+s[3]
+s[4]
+s[5]
+
+# List
+names = ['venu', 'tarun', 'bhuvan', 'manoj']
+names.append('manoj')
+
+# Tuple
+t = (1, 1, 1, 1, 2, 3, 4, 5)
+t[0]
+t[2]
+
+# Set
+s = {56789, 5467, 123, 34, 1, 387, 7}
+s.add(1)
+
+# Dictionary
+products = {'product_id': 1, "product_name": "laptop", "price": 75000}
+
+# Boolean
+status = True
+
+# Type conversions (int)
+a = None
+a = 10
+float(a)
+str(a)
+complex(a)
+bool(a)
+
+# Float conversions
+a = 12.3
+int(a)
+str(a)
+complex(a)
+bool(a)
+
+# Complex conversions
+a = 4 + 2j
+str(a)
+bool(a)
+
+# List conversions
+l = [1, 2, 3, 4, 4, 5, 6]
+tuple(l)
+set(l)
+bool(l)
+
+# Tuple conversions
+t = (1, 2, 3, 4)
+list(t)
+set(t)
+bool(t)
+
+# Set conversions
+s = {1, 2, 3, 4, 5, 6}
+list(s)
+tuple(s)
+bool(s)
+
+# Dictionary conversions
+d = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+str(d)
+list(d)
+tuple(d)
+set(d)
+bool(d)
+
+# Boolean conversions
+a = True
+int(a)
+float(a)
+str(a)
+bool(a)
+
