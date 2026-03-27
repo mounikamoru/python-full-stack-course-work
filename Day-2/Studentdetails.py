61e07085c6b0cce6d3e4582f3416164af6d272ae
mounikamoru <mounikamoru4634@gmail.com>
2026-03-27 16:52:46 +0530

    Add files via upload

---
+++ Day-2/Studentdetails.py
@@ -0,0 +1,29 @@
+#student_details
+Student_ID = 'CGH2884'
+Batch_No='PFS-HYD-050'
+Email_ID= 'tejav7236@gmail.com'
+Age=23
+State='Anantapur'
+Location='hyderabad'
+Student_No='+916302568815'
+Parent_No='+918897431688'
+Github='https://github.com/Cvenkatteja'
+College_Name='Gnanamani college of technology'
+USN_Number=620822106008
+Department='B.Tech - Electronics and Communication Engineering (ECE)'
+Qualification='UG (Bachelor Degrees)'
+Graduation ='76%'
+Year_of_Passing= 2026
+Skills=('Python', 'Flask', 'HTML', 'CSS')
+Mode_of_Study='Offline'
+
+print('------------Personal Information----------------')
+print(f'Student ID: {Student_ID}')
+print(f'Batch No: {Batch_No}')
+print(f'Email ID: {Email_ID}')
+
+
+print('------------Academic Information----------------')
+print(f'Department: {Department}')
+print(f'College Name: {College_Name}')
+print(f'Qualification: {Qualification}')
