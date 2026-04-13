aab588900480efe3666767864b39e534cd0ee5fe
mounikamoru <mounikamoru4634@gmail.com>
2026-04-13 22:45:47 +0530

    Add files via upload

---
+++ Day-1/Student_details.py
@@ -0,0 +1,39 @@
+Student_Name = "Avusali Anjusree"
+Student_ID = "CGH2936"
+Batch_No = "PFS-HYD-050"
+Email_ID = "anjusreeavusali@gmail.com"
+Date_of_Birth = "2004-01-01"
+Age = 22
+Gender = "Female"
+Blood_Group = "A+"
+City = "Sangareddy Dist."
+State = "Telangana"
+Student_Phone_Number = "+918919177798"
+Parent_Phone_Number = "+919515624066"
+Github_Link = "https://github.com/Anjusree01"
+print("-----------------------------------Academic_Information---------------------------------")
+College_Name = ("Sreenidhi Institute of Science and Technology")
+USN_Number = "22311a04bd"
+Qualification = "UG (Bachelor Degrees)"
+Department = "B.Tech - Electronics and Communication Engineering (ECE)"
+Pass_out_Year = 2026
+Graduation_Percentage = "83.5%"
+Arrears = "No"
+No_of_Arrears = 0
+_10th_Pass_Year = 2020
+_10th_Percentage = "100%"
+_12th_Pass_Year = 2014
+_12th_Percentage = "97.2%"
+Skills = ['Python', 'Flask', 'HTML', 'CSS', 'Bootstrap', 'Javascript', 'MySQL']
+Mode_of_Study='Offline'
+
+print('------------Personal Information----------------')
+print(f'Student ID: {Student_ID}')
+print(f'Batch No: {Batch_No}')
+print(f'Email ID: {Email_ID}')
+
+
+print('------------Academic Information----------------')
+print(f'Department: {Department}')
+print(f'College Name: {College_Name}')
+print(f'Qualification: {Qualification}')
---
+++ Day-1/print_stmt.py
@@ -0,0 +1,4 @@
+print("NAME: Anjusree")
+print("REG NO.: 50-30")
+print("BATCH NO.: 50")
+print("SKILLS: Python, MySQL, Django, Flask")
